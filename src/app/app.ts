d4e729ba80d0c47b361b3961a69f8a9b8cd05374
John Hedderman <john.hedderman.3@gmail.com>
2025-09-02 09:22:33 -0400

    Components cleanup and moves

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
-import { Navbar } from './page/navbar/navbar';
+import { Navbar } from './components/navbar/navbar';
 
 @Component({
   selector: 'app-root',
