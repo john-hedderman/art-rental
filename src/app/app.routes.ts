22952ba2ccbdf67828e454884272e430f4b54f79
John Hedderman <john.hedderman.3@gmail.com>
2025-09-04 10:39:42 -0400

    Reorganized pages files

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
 
 import { Home } from './page/home/home';
-import { ArtPage } from './page/art-page/art-page';
-import { ArtistsPage } from './page/artists-page/artists-page';
-import { ClientsPage } from './page/clients-page/clients-page';
+import { ArtPage } from './page/art/art-page';
+import { ArtistsPage } from './page/artists/artists-page';
+import { ClientsPage } from './page/clients/clients-page';
 
 export const routes: Routes = [
   {
