6821d757e5cf91dd7c35e9fd09b38219096a3b4c
John Hedderman <john.hedderman.3@gmail.com>
2025-09-08 10:08:15 -0400

    Use Bootstrap grid in Client list, enhance client data a little

--- src/app/model/models.ts
+++ src/app/model/models.ts
@@ -15,6 +15,11 @@ export interface Artist {
 export interface Client {
   id: number;
   name: string;
+  address1: string;
+  address2: string;
+  city: string;
+  state: string;
+  zipCode: string;
   industry: string;
 }
 
