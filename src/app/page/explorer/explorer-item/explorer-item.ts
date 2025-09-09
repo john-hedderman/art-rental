f4b27a4153a2d208cbedb1ddf80024f330143789
John Hedderman <john.hedderman.3@gmail.com>
2025-09-09 10:03:16 -0400

    Removed unused component Explorer

--- src/app/page/explorer/explorer-item/explorer-item.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-explorer-item',
-  templateUrl: './explorer-item.html',
-  styleUrl: './explorer-item.scss',
-})
-export class ExplorerItem {}
