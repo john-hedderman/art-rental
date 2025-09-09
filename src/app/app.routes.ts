c5763b2dd440edf14cb620f14aec4541d5292924
John Hedderman <john.hedderman.3@gmail.com>
2025-09-09 08:06:04 -0400

    Get rid of unused Home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,20 +1,15 @@
 import { Routes } from '@angular/router';
 
-import { Home } from './page/home/home';
 import { ArtPage } from './page/art/art-page';
+import { ArtList } from './page/art/art-list/art-list';
 import { ArtistsPage } from './page/artists/artists-page';
+import { ArtistList } from './page/artists/artist-list/artist-list';
 import { ClientsPage } from './page/clients/clients-page';
 import { ClientList } from './page/clients/client-list/client-list';
 import { AddClient } from './page/clients/add-client/add-client';
 import { ClientDetail } from './page/clients/client-detail/client-detail';
-import { ArtistList } from './page/artists/artist-list/artist-list';
-import { ArtList } from './page/art/art-list/art-list';
 
 export const routes: Routes = [
-  {
-    path: 'home',
-    component: Home,
-  },
   {
     path: 'art',
     component: ArtPage,
