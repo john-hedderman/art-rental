fda4bd77e6a3a7a3f86439255079bbac8e0c1cd9
John Hedderman <john.hedderman.3@gmail.com>
2025-09-26 10:20:18 -0400

    Add address to Job list

--- src/app/features/jobs/job-list/job-list.ts
+++ src/app/features/jobs/job-list/job-list.ts
@@ -24,6 +24,7 @@ import { PageHeader } from '../../../shared/components/page-header/page-header';
 })
 export class JobList implements OnInit {
   @ViewChild('clientNameTemplate', { static: true }) clientNameTemplate!: TemplateRef<any>;
+  @ViewChild('jobAddressTemplate', { static: true }) jobAddressTemplate!: TemplateRef<any>;
 
   navigateToAddJob = () => {
     this.router.navigate(['/jobs', 'add']);
@@ -60,6 +61,12 @@ export class JobList implements OnInit {
     this.navigateToJobDetail(selected[0].id);
   }
 
+  addressComparator(rowA: any, rowB: any): number {
+    const locationA = `${rowA['address1']}, ${rowA['city']}, ${rowA['state']} ${rowA['zipCode']}`;
+    const locationB = `${rowB['address1']}, ${rowB['city']}, ${rowB['state']} ${rowB['zipCode']}`;
+    return locationA.localeCompare(locationB);
+  }
+
   constructor(private dataService: DataService, private router: Router) {
     combineLatest([this.dataService.clients$, this.dataService.jobs$]).subscribe(
       ([clients, jobs]) => {
@@ -79,7 +86,13 @@ export class JobList implements OnInit {
   ngOnInit(): void {
     this.columns = [
       { prop: 'id', name: 'Job Number' },
-      { name: 'Client', cellTemplate: this.clientNameTemplate },
+      { prop: '', name: 'Client', cellTemplate: this.clientNameTemplate },
+      {
+        prop: '',
+        name: 'Address',
+        cellTemplate: this.jobAddressTemplate,
+        comparator: this.addressComparator,
+      },
     ];
   }
 }
