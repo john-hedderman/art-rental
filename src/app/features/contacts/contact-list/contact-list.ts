875bfc39095ebf1976f89d1c75948f60aa0fdea5
John Hedderman <john.hedderman.3@gmail.com>
2025-10-02 10:00:59 -0400

    Add responsive row detail to Contacts list page

--- src/app/features/contacts/contact-list/contact-list.ts
+++ src/app/features/contacts/contact-list/contact-list.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
-import { NgxDatatableModule, TableColumn } from '@swimlane/ngx-datatable';
+import { Component, HostListener, OnInit, TemplateRef, ViewChild } from '@angular/core';
+import { DatatableComponent, NgxDatatableModule, TableColumn } from '@swimlane/ngx-datatable';
+import { combineLatest } from 'rxjs';
 
 import { PageHeader } from '../../../shared/components/page-header/page-header';
 import { Client, Contact, HeaderData } from '../../../model/models';
 import { DataService } from '../../../service/data-service';
-import { combineLatest } from 'rxjs';
 
 @Component({
   selector: 'app-contact-list',
@@ -17,8 +17,19 @@ import { combineLatest } from 'rxjs';
   },
 })
 export class ContactList implements OnInit {
+  @ViewChild('contactsTable') table!: DatatableComponent<Contact>;
+  @ViewChild('arrowTemplate', { static: true }) arrowTemplate!: TemplateRef<any>;
   @ViewChild('nameTemplate', { static: true }) nameTemplate!: TemplateRef<any>;
+  @ViewChild('clientNameHeaderTemplate', { static: true })
+  clientNameHeaderTemplate!: TemplateRef<any>;
   @ViewChild('clientNameTemplate', { static: true }) clientNameTemplate!: TemplateRef<any>;
+  @ViewChild('phoneHeaderTemplate', { static: true }) phoneHeaderTemplate!: TemplateRef<any>;
+  @ViewChild('phoneTemplate', { static: true }) phoneTemplate!: TemplateRef<any>;
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event) {
+    this.checkScreenSize();
+  }
 
   contacts: Contact[] = [];
 
@@ -29,6 +40,26 @@ export class ContactList implements OnInit {
 
   rows: Contact[] = [];
   columns: TableColumn[] = [];
+  expanded: any = {};
+
+  checkScreenSize() {
+    let detailRows;
+    if (window.innerWidth >= 768) {
+      detailRows = document.querySelectorAll('.datatable-row-detail:not(.d-none)');
+      detailRows.forEach((detailRow) => {
+        detailRow.classList.add('d-none');
+      });
+    } else {
+      detailRows = document.querySelectorAll('.datatable-row-detail.d-none');
+      detailRows.forEach((detailRow) => {
+        detailRow.classList.remove('d-none');
+      });
+    }
+  }
+
+  toggleExpandRow(row: Contact) {
+    this.table.rowDetail!.toggleExpandRow(row);
+  }
 
   nameComparator(valueA: any, valueB: any, rowA: any, rowB: any): number {
     const nameA = `${rowA['firstName']} ${rowA['lastName']}`;
@@ -58,16 +89,33 @@ export class ContactList implements OnInit {
   ngOnInit(): void {
     this.columns = [
       {
+        width: 50,
+        resizeable: false,
+        sortable: false,
+        draggable: false,
+        canAutoResize: false,
+        cellTemplate: this.arrowTemplate,
+      },
+      {
+        width: 250,
         name: 'Name',
         cellTemplate: this.nameTemplate,
         comparator: this.nameComparator,
       },
       {
+        width: 200,
         name: 'Client',
+        headerTemplate: this.clientNameHeaderTemplate,
         cellTemplate: this.clientNameTemplate,
         comparator: this.clientNameComparator,
       },
-      { prop: 'phone', name: 'Phone' },
+      {
+        width: 150,
+        prop: 'phone',
+        name: 'Phone',
+        headerTemplate: this.phoneHeaderTemplate,
+        cellTemplate: this.phoneTemplate,
+      },
     ];
   }
 }
