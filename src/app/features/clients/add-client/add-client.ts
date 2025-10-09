5db8c531864c7e766d6b9b4fb14da49e3aa70beb
John Hedderman <john.hedderman.3@gmail.com>
2025-10-09 12:28:06 -0400

    Begin Add Client form

--- src/app/features/clients/add-client/add-client.ts
+++ src/app/features/clients/add-client/add-client.ts
@@ -1,33 +1,49 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
+
 import { HeaderData } from '../../../model/models';
 import { PageHeader } from '../../../shared/components/page-header/page-header';
 
 @Component({
   selector: 'app-add-client',
-  imports: [PageHeader],
+  imports: [PageHeader, ReactiveFormsModule],
   templateUrl: './add-client.html',
   styleUrl: './add-client.scss',
   standalone: true,
 })
-export class AddClient {
-  navigateToClientList = () => {
+export class AddClient implements OnInit {
+  goToClientList = () => {
     this.router.navigate(['/clients', 'list']);
   };
   headerData: HeaderData = {
     headerTitle: 'Add Client',
     headerButtons: [
       {
-        id: 'returnToClientListBtn',
+        id: 'goToClientListBtn',
         label: 'Client list',
         type: 'button',
         buttonClass: 'btn btn-primary btn-sm',
         disabled: false,
-        clickHandler: this.navigateToClientList,
+        clickHandler: this.goToClientList,
       },
     ],
   };
 
+  clientForm!: FormGroup;
+
   constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.clientForm = new FormGroup({
+      clientName: new FormControl(''),
+      address: new FormGroup({
+        address1: new FormControl(''),
+        address2: new FormControl(''),
+        city: new FormControl(''),
+        state: new FormControl(''),
+        zip: new FormControl(''),
+      }),
+    });
+  }
 }
