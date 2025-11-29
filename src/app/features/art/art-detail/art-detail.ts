b936338c723b024764dd71cbac0ada0567c5be2b
John Hedderman <john.hedderman.3@gmail.com>
2025-11-29 14:31:06 -0500

    Show job site information on Art Detail page

--- src/app/features/art/art-detail/art-detail.ts
+++ src/app/features/art/art-detail/art-detail.ts
@@ -134,7 +134,7 @@ export class ArtDetail implements OnDestroy {
             if (client) {
               job = { ...job, client };
             }
-            const site = sites.find((site) => site.job_id === job?.job_id);
+            const site = sites.find((site) => site.site_id === job?.site_id);
             if (site) {
               job = { ...job, site };
             }
