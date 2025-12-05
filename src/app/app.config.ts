1c25721d975882b0e3e9d19cc18b339c380da1a8
John Hedderman <john.hedderman.3@gmail.com>
2025-12-05 09:33:14 -0500

    Uninstall ngrx, which is probably overkill

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,9 +7,6 @@ import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
-import { provideStore } from '@ngrx/store';
-import { provideRouterStore } from '@ngrx/router-store';
-import { provideEffects } from '@ngrx/effects';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -17,8 +14,5 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(),
-    provideStore(),
-    provideRouterStore(),
-    provideEffects()
-],
+  ],
 };
