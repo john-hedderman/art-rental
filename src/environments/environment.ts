2f1b71a2aea980c8b40d75084a66aa48ff4f99a9
John Hedderman <john.hedderman.3@gmail.com>
2026-02-11 17:17:12 -0500

    Explain in environment file which hosts are meant for what

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,11 @@
+// set to localhost when running unit tests
+// ensure that the front end app is running via "ng serve"
+// ensure that the back end node app is also running on localhost
+
+// when wanting local mobile access, set to local IPv4 address
+// ensure that the front end app is running via "npm run start:lan" (on 0.0.0.0)
+// ensure that the back end node app is running on host 0.0.0.0
+
 export const environment = {
-  apiUrl: 'http://192.168.1.179:3000',
+  apiUrl: 'http://192.168.1.179:3000'
 };
