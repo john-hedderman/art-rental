2a819bcc1adf7bc1acce56f3420fa68d9da6ef76
John Hedderman <john.hedderman.3@gmail.com>
2026-08-01 16:29:55 -0400

    Add script to run npx serve for local production build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
+// when you want local mobile access or access from other device on the same network as the server,
+// set localhost to the local IPV4 address instead
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:3000',
+  apiUrl: 'http://localhost:3000'
 };
