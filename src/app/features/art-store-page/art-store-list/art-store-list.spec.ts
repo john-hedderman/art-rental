2d84a656743afff7fda080f13f375358dbb57cc7
John Hedderman <john.hedderman.3@gmail.com>
2026-08-10 17:52:43 -0400

    Upgrade v22 branch to intermediate Angular version 21

--- src/app/features/art-store-page/art-store-list/art-store-list.spec.ts
+++ src/app/features/art-store-page/art-store-list/art-store-list.spec.ts
@@ -1,16 +1,24 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideMockStore, MockStore } from '@ngrx/store/testing';
 
 import { ArtStoreList } from './art-store-list';
 
 describe('ArtStoreList', () => {
   let component: ArtStoreList;
   let fixture: ComponentFixture<ArtStoreList>;
+  let store: MockStore;
+
+  const initialState = {
+    data: {}
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ArtStoreList]
+      imports: [ArtStoreList],
+      providers: [provideMockStore({ initialState })]
     }).compileComponents();
 
+    store = TestBed.inject(MockStore);
     fixture = TestBed.createComponent(ArtStoreList);
     component = fixture.componentInstance;
     fixture.detectChanges();
--- src/app/features/store-page/store-page.spec.ts
+++ src/app/features/store-page/store-page.spec.ts
@@ -1,17 +1,24 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideMockStore, MockStore } from '@ngrx/store/testing';
 
 import { StorePage } from './store-page';
 
 describe('StorePage', () => {
   let component: StorePage;
   let fixture: ComponentFixture<StorePage>;
+  let store: MockStore;
+
+  const initialState = {
+    data: {}
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [StorePage]
-    })
-    .compileComponents();
+      imports: [StorePage],
+      providers: [provideMockStore({ initialState })]
+    }).compileComponents();
 
+    store = TestBed.inject(MockStore);
     fixture = TestBed.createComponent(StorePage);
     component = fixture.componentInstance;
     fixture.detectChanges();
