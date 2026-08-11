bfdf036c4ddf157f814c6293a0b53a742ffa7939
John Hedderman <john.hedderman.3@gmail.com>
2026-08-11 11:02:08 -0400

    Provided reducer in app config for test ngrx counter page

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,6 +7,7 @@ import { provideRouter } from '@angular/router';
 import { provideHttpClient, withXhr } from '@angular/common/http';
 import { provideStore } from '@ngrx/store';
 import { provideEffects } from '@ngrx/effects';
+import { counterReducer } from '../app/features/store-page/+state/store-page.reducer';
 import { artReducer } from '../app/features/art-store-page/+state/art-store-page.reducer';
 import { routes } from './app.routes';
 import { ArtEffects } from './features/art-store-page/+state/art-store-page.effects';
@@ -17,7 +18,7 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(withXhr()),
-    provideStore({ art: artReducer }),
+    provideStore({ art: artReducer, counter: counterReducer }),
     provideEffects([ArtEffects])
   ]
 };
